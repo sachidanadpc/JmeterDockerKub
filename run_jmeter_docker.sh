868ea83b1b17db3ec7a8589d30669bb34b6967bc
sachidanadpc <49983832+sachidanadpc@users.noreply.github.com>
2024-03-01 23:20:30 +0530

    Update run_jmeter_docker.sh

--- run_jmeter_docker.sh
+++ run_jmeter_docker.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 # Script to run JMeter tests locally with Docker
 
-docker run --rm -v /tests/scripts/testplan.jmx:/jmeter/testplan.jmx justb4/jmeter:latest -n -t /jmeter/testplan.jmx -l /jmeter/testplan.jtl
+# Specify the path to your JMeter test plan file
+TEST_PLAN_PATH="/path/to/your/testplan.jmx"
+
+# Check if the test plan file exists
+if [ ! -f "$TEST_PLAN_PATH" ]; then
+    echo "Error: The file $TEST_PLAN_PATH doesn't exist or can't be opened"
+    exit 1
+fi
+
+# Run JMeter tests with Docker
+docker run --rm -v "$TEST_PLAN_PATH:/jmeter/testplan.jmx" justb4/jmeter:latest -n -t /jmeter/testplan.jmx -l /jmeter/testplan.jtl
